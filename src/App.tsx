a3e509c911f0eeb3975f5b2ae51b0792e4e8eeb7
Agrapujya Lashkari <joebrion20@gmail.com>
2024-12-31 19:42:08 +0530

    added some rafce pages and added some layouts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
-import { useState } from 'react'
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import SignupForm from "./auth/forms/SignupForm";
+import SigninForm from "./auth/forms/SigninForm";
+import Home from "./root/pages/Home";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-      <div className=''>App</div>
+      <main className=" flex h-screen">
+        <Routes>
+          {/* publicRoutes */}
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+          {/* privateRoutes */}
+          <Route index element={<Home />} />
+        </Routes>
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
